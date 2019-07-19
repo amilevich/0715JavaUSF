4b85cf5de265ace716f27b49ae25b37d7a51daed
Nick Helgeland <nickhelgeland94@gmail.com>
2019-07-19 10:32:31 -0400

    Changed the Accounts class to map a customer id to each account.

--- Project_0/src/main/java/com/revature/nick/database/Accounts.java
+++ Project_0/src/main/java/com/revature/nick/database/Accounts.java
@@ -1,6 +1,36 @@
 package com.revature.nick.database;
 
+import java.util.HashMap;
+
+import com.revature.nick.model.Account;
+
 public class Accounts
 {
-
+	private static Accounts instance = null;
+	
+	private HashMap<Integer, Account> accountMap;
+	
+	private Accounts()
+	{
+		accountMap = new HashMap<Integer, Account>();
+	}
+	
+	public Accounts getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new Accounts();
+		}
+		return instance;
+	}
+	
+	public void addAccount(Account account, int customerID)
+	{
+		this.accountMap.put(customerID, account);
+	}
+	
+	public Account getAccount(int id)
+	{
+		return this.accountMap.get(id);
+	}
 }
