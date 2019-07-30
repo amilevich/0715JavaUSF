99fd55009a00a1937ad5ae762db048fe8e94bff2
Nick Helgeland <nickhelgeland94@gmail.com>
2019-07-30 09:31:09 -0400

    Added an employee class.

---
+++ BankingApp/src/main/kotlin/com/example/demo/model/Employee.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+public class Employee extends User
+{
+    public Employee(String username, String password)
+    {
+        super(username, password);
+    }
+
+    public void approveApplication(AccountApplication application)
+    {
+
+    }
+}
