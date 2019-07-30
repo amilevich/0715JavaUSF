af798cae280449584f8b0ac2c4c1927930c57c76
Andre Doucet <doucetandre909@gmail.com>
2019-07-30 12:58:10 -0400

    All homework is complete: except for q18

--- Andre_Doucet_Code/Java Core Homework Andre Doucet/src/q19/ArrayMathMalarky.java
+++ Andre_Doucet_Code/Java Core Homework Andre Doucet/src/q19/ArrayMathMalarky.java
@@ -9,15 +9,63 @@
 package q19;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class ArrayMathMalarky {
 	public static void main(String[] args) {
-	ArrayList<Integer> boop = new ArrayList<Integer>(10);
+	ArrayList<Integer> boop = new ArrayList<Integer>();
+	int even = 0, sum = 0;
 	
 	for (int i = 0; i <= 10; i++)
 	{
+		
 		boop.add(i);
-		System.out.println(boop.get(i));
+		System.out.print(boop.get(i)+ " ");
 	}
 	
-}}
+	for (int e=0; e < boop.size(); e++) {
+		if((e%2)==0) {
+			even+=boop.get(e);
+			
+		} else {
+			sum +=boop.get(e);
+		}
+	}
+	
+	
+	
+	System.out.println("\n" + even);
+	System.out.println("\n" + sum);
+	boop = checkPrime(boop);
+	System.out.println(boop);
+	
+	
+	
+	
+	
+}
+	static ArrayList<Integer> checkPrime(ArrayList<Integer> arr) {
+		ArrayList<Integer> primeNumero = new ArrayList<>();
+
+		ListIterator<Integer> iterator = arr.listIterator();
+
+		while (iterator.hasNext()) {
+			int numba = iterator.next();
+			boolean snitch = false;
+			// Verify if the number is prime
+			for (int i = 2; i < numba; i++) {
+				if ((numba % i) == 0) {
+					snitch = true;
+					break;
+				}
+			}
+			
+			//if the condition for the number being prime is met, it is taken our of to the prime number list
+			if (snitch) {
+				primeNumero.add(numba);
+			}
+		}
+		return primeNumero;
+}
+	
+}
