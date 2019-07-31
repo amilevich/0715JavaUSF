b447bca94710da9872c9e6e89d1df114f981b982
Andre Doucet <doucetandre909@gmail.com>
2019-07-31 19:02:21 -0400

    All homework is finished

--- Andre_Doucet_Code/Java Core Homework Andre Doucet/src/q17/PrincipalPrinciple.java
+++ Andre_Doucet_Code/Java Core Homework Andre Doucet/src/q17/PrincipalPrinciple.java
@@ -1,4 +1,5 @@
-//Write a program that calculates the simple interest on the principal, rate of interest and number of years provided by the user. 
+//Write a program that calculates the simple interest on the principal, rate of interest and number of years 
+//provided by the user. 
 //Enter principal, rate and time through the console using the Scanner class.
 //Interest = Principal* Rate* Time
 
