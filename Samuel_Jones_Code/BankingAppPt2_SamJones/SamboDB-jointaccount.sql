b27f9fc3140be3f2c43571a150085b2e16ee82d8
Sam Jones <sjonesmusician@gmail.com>
2019-08-12 21:14:58 -0400

    edited the tables for the varchars to be larger

--- Samuel_Jones_Code/BankingAppPt2_SamJones/SamboDB-jointaccount.sql
+++ Samuel_Jones_Code/BankingAppPt2_SamJones/SamboDB-jointaccount.sql
@@ -1,11 +1,31 @@
 --DROP TABLE accounts; -- DDL data definition language ALTER CREATE TRUNCATE DROP
 --DROP TABLE users;
 --DROP TABLE junctiontable;
+
 -- run these three below to reset the sequences to 1, and empty the tables
 DROP SEQUENCE aid_sequence; 
 truncate table junctiontable;
 truncate table  accounts;
 truncate table users;
+ 
+CREATE TABLE accounts(
+    aid VARCHAR2(30) PRIMARY KEY,
+    balance NUMBER(20) NOT NULL,
+    isapproved NUMBER(1) NOT NULL,
+    temptoken VARCHAR(30) UNIQUE
+    );
+--ALTER TABLE accounts(
+    
+CREATE TABLE users(
+    username VARCHAR2(30) PRIMARY KEY,
+    password VARCHAR2(30) NOT NULL
+    );
+    
+CREATE TABLE junctiontable(
+    username VARCHAR2(30),
+    aid VARCHAR2(30)
+    );
+    
 CREATE SEQUENCE aid_sequence
     START WITH 1
     INCREMENT BY 1;
@@ -18,55 +38,8 @@ CREATE SEQUENCE aid_sequence
         IF: new.aid IS NULL THEN
         SELECT aid_sequence.nextval INTO :new.aid FROM dual;
         END IF;
-    END;  
-CREATE TABLE accounts(
-    aid VARCHAR2(10) PRIMARY KEY,
-    balance NUMBER(20) NOT NULL,
-    isapproved NUMBER(1) NOT NULL,
-    temptoken VARCHAR(20) UNIQUE
-    );
---ALTER TABLE accounts(
-    
-CREATE TABLE users(
-    username VARCHAR2(10) PRIMARY KEY,
-    password VARCHAR2(10) NOT NULL
-    );
-    
-CREATE TABLE junctiontable(
-    username VARCHAR2(10),
-    aid VARCHAR2(10)
-    );
---CREATE TABLE lookupTableJoint(
---    username1 VARCHAR2(10),
---    username2 VARCHAR2(10),
---    aid VARCHAR2(10)
---    );
+    END; 
 
-
---CREATE SEQUENCE aid_sequence2
---    start with 1
---    increment by 1;
---CREATE OR REPLACE TRIGGER increment_AID_Lookup
---    BEFORE INSERT ON lookuptable
---    FOR EACH ROW
---    BEGIN
---       IF: new.aid IS NULL THEN
---        SELECT aid_sequence.nextval INTO :new.aid FROM dual;
---        END IF;
---    END; 
- 
-    
- INSERT INTO users VALUES ('admin','admin');
---INSERT INTO lookuptable VALUES ('admin',null);
-INSERT INTO users VALUES ('emp','emp');
-INSERT INTO users VALUES ('test','test');
---INSERT INTO lookuptable VALUES ('emp',null);
 SELECT * FROM users;
 SELECT * FROM accounts;
 SELECT * FROM junctiontable;
---INSERT INTO lookuptable VALUES ('sam',null);
-
---select * from accounts;
---INSERT INTO accounts (aid, balance,isApproved) VALUES (null, 100, F);
---DELETE FROM accounts WHERE aid ='1';
---DELETE FROM accounts WHERE aid = '2';
