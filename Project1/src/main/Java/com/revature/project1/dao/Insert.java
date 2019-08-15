662e610a68cbdb7e97d855343475008560d219f8
Nick Helgeland <nickhelgeland94@gmail.com>
2019-08-15 12:33:09 -0400

    deleted project one and moved it to a different repository.

--- Project1/src/main/Java/com/revature/project1/dao/Insert.java
+++
@@ -1,6 +0,0 @@
-package com.revature.project1.dao;
-
-public interface Insert<T>
-{
-    void insert(T obj);
-}
--- Project1/src/main/Java/com/revature/project1/dao/Update.java
+++
@@ -1,6 +0,0 @@
-package com.revature.project1.dao;
-
-public interface Update<T>
-{
-    void update(T obj);
-}
