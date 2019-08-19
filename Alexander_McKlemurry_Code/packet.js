3853c14c14f825128e0c8f6fb301615d0aaf1584
Alexander McKlemurry <attacusmcklemurry@gmail.com>
2019-08-19 06:03:02 -0500

    Add files via upload

--- Alexander_McKlemurry_Code/packet.js
+++ Alexander_McKlemurry_Code/packet.js
@@ -6,127 +6,127 @@
 //Define function: fib(n) 
 //Return the nth number in the fibonacci sequence.
 
-//function fib(n)
-//{
-//var n1= 0;
-//var n2 = 1;
-//var val;
-//document.write(var1+"<br />");
-//document.write(var2+"<br />");
-//for(var i=3; i <= n;i++)
-//{
-//val = n1 + n2
-//n1 = n2
-//n2 = val
-//document.write(val+"<br />");
-//}
-//}
-//var no = prompt(" nth",0);
-//console.log(fib(no))
+function fib(n)
+{
+var n1= 0;
+var n2 = 1;
+var val;
+document.write(var1+"<br />");
+document.write(var2+"<br />");
+for(var i=3; i <= n;i++)
+{
+val = n1 + n2
+n1 = n2
+n2 = val
+document.write(val+"<br />");
+}
+}
+var no = prompt(" nth",0);
+console.log(fib(no))
 
 //2. Bubble Sort
 //Define function: bubbleSort(numArray)
 //Use the bubble sort algorithm to sort the array.
 //Return the sorted array.
 
-//function swap(arr, Index1, Index2){
-//var temp = arr[Index1];
-//arr[Index1] = arr[Index2];
-//arr[Index2] = temp;
-//}
-//
-//function bubbleSort(arr){
-//var len = arr.length,
-//i, j, stop;
-//for (i=0; i < len; i++){
-//for (j=0, stop=len-i; j < stop; j++){
-//if (arr[j] > arr[j+1]){
-//swap(arr, j, j+1);
-//}
-//}
-//}
-//document.write(arr+"<br />");
-//}
-//bubbleSort([3, 0, 2, -2, -1, 4, 1]);
+function swap(arr, Index1, Index2){
+let temp = arr[Index1];
+arr[Index1] = arr[Index2];
+arr[Index2] = temp;
+}
+
+function bubbleSort(arr){
+let len = arr.length,
+i, j, stop;
+for (i=0; i < len; i++){
+for (j=0, stop=len-i; j < stop; j++){
+if (arr[j] > arr[j+1]){
+swap(arr, j, j+1);
+}
+}
+}
+document.write(arr+"<br />");
+}
+bubbleSort([3, 0, 2, -2, -1, 4, 1]);
 
 //3. Reverse String
 //Define function: reverseStr(someStr)
 //Reverse and return the String.
 
-//function reverseStr(str) {
-//var splitString = str.split("");
-//var reverseArray = splitString.reverse();
-//var joinArray = reverseArray.join("");
-//document.write(joinArray+"<br />");
-//}
-//reverseStr("hello");
+function reverseStr(str) {
+var splitString = str.split("");
+var reverseArray = splitString.reverse();
+var joinArray = reverseArray.join("");
+document.write(joinArray+"<br />");
+}
+reverseStr("hello");
 
 //4. Factorial
 //Define function: factorial(someNum)
 //Use recursion to compute and return the factorial of someNum.
 
-//function factorial(n){
-//var i, fact;
-//fact=1;
-//for(i=1; i<=n; i++)
-//{
-//fact= fact*i;
-//}
-//document.write(fact+"<br />");
-//}
-//factorial(5)
+function factorial(n){
+let i, fact;
+fact=1;
+for(i=1; i<=n; i++)
+{
+fact= fact*i;
+}
+document.write(fact+"<br />");
+}
+factorial(5)
 
 //5. Substring
 //Define function substring(someStr, length, offset)
 //Return the substring contained between offset and (offset + length) inclusively.
 //If incorrect input is entered, use the alert function and describe why the input was incorrect.
 
-//function substring(someStr, length, offset){
-//   if(offset < 0){
-//      alert("Offset is less than zero");
-//    }
-//   else if(offset+length > someStr.length){
-//      alert("Offset is greater than length of the string");
-//    }
-//   else{
-//      return str.substr(offset, offset+length);
-//  }
-//}
+function substring(someStr, length, offset){
+   if(offset < 0){
+      alert("Offset is less than zero");
+    }
+   else if(offset+length > someStr.length){
+      alert("Offset is greater than length of the string");
+    }
+   else{
+      return str.substr(offset, offset+length);
+  }
+}
 //console.log(str.substr);
 //6. Even Number
 //Define function: isEven(someNum)
 //Return true if even, false if odd.
 //Do not use % operator.
 
-//function isEven(n) {
-//   return /^-?\d*[02468]$/.test(n);
-//}
-//
-//function isOdd(n) {
-//   return /^-?\d*[13579]$/.test(n);
-//}
-//console.log(n)
+function isEven(n) {
+   return /^-?\d*[02468]$/.test(n);
+}
+
+function isOdd(n) {
+   return /^-?\d*[13579]$/.test(n);
+}
+console.log(n)
 
 //7. Palindrome
 //Define function isPalindrome(someStr)
 //Return true if someStr is a palindrome, otherwise return false
 
-//function checkPalindrome(word) {    
-//    var l = word.length;
-//    for (var i = 0; i < l / 2; i++) {
-//       if (word.charAt(i) !== word.charAt(l - 1 - i)) {
-//            return false;
-//        }
-//    }
-//    return true;
-//}
+function checkPalindrome(word) {    
+    let l = word.length;
+    for (let i = 0; i < l / 2; i++) {
+      if (word.charAt(i) !== word.charAt(l - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
+}
 
-//if (checkPalindrome("1122332211")) {
-//  document.write("The word is a palindrome");
-//} else {
-//    document.write("The word is NOT a palindrome");
-//}
-//console.log(length)
+if (checkPalindrome("1122332211")) {
+  document.write("The word is a palindrome");
+} else {
+    document.write("The word is NOT a palindrome");
+}
+console.log(length)
 
 
 //8. Shapes
@@ -152,61 +152,61 @@
 //  *
 
 
-//function printShape(s, n, c) {
-//switch(s) {
-//case "Square":
-//for(let i=0;i<n;i++) {
-//for(let j=0;j<n;j++) {
-//document.write(c);
-//console.log(c);
-//}
-//document.write("<br>");
-//console.log("<br>");
-//}
-//break;
-//case "Triangle":
-//for(let i=0;i<n;i++) {
-//for(let j=0;j<=i;j++) {
-//document.write(c);
-//console.log(c);
-//}
-//document.write("<br>");
-//console.log("<br>");
-//}
-//break;
-//case "Diamond":
-//for(let i=0;i<n;i+=2) {
-//for(let k=0;k<=n/2-i;k++) {
-//document.write("&nbsp");
-//console.log(" ");
-//}
-//for(let j=0;j<=i;j++) {
-//document.write(c);
-//console.log(c);
-//}
-//document.write("<br>");
-//console.log("<br>");
-//}
-//for(var i=n-2;i>=0;i-=2) {
-//for(var k=0;k<=(n-i)/2;k++) {
-//document.write("&nbsp");
-//console.log(" ");
-//}
-//for(var j=0;j<i;j++) {
-//document.write(c);
-//console.log(c);
-//}
-//document.write("<br>");
-//console.log("<br>");
-//}
-//break;
-//}
-//
-//}
-//
-//printShape("Square", 3, "%");
-//printShape("Triangle", 3, "$");
-//printShape("Diamond", 5, "*");
+function printShape(s, n, c) {
+switch(s) {
+case "Square":
+for(let i=0;i<n;i++) {
+for(let j=0;j<n;j++) {
+document.write(c);
+console.log(c);
+}
+document.write("<br>");
+console.log("<br>");
+}
+break;
+case "Triangle":
+for(let i=0;i<n;i++) {
+for(let j=0;j<=i;j++) {
+document.write(c);
+console.log(c);
+}
+document.write("<br>");
+console.log("<br>");
+}
+break;
+case "Diamond":
+for(let i=0;i<n;i+=2) {
+for(let k=0;k<=n/2-i;k++) {
+document.write("&nbsp");
+console.log(" ");
+}
+for(let j=0;j<=i;j++) {
+document.write(c);
+console.log(c);
+}
+document.write("<br>");
+console.log("<br>");
+}
+for(let i=n-2;i>=0;i-=2) {
+for(let k=0;k<=(n-i)/2;k++) {
+document.write("&nbsp");
+console.log(" ");
+}
+for(let j=0;j<i;j++) {
+document.write(c);
+console.log(c);
+}
+document.write("<br>");
+console.log("<br>");
+}
+break;
+}
+
+}
+
+printShape("Square", 3, "%");
+printShape("Triangle", 3, "$");
+printShape("Diamond", 5, "*");
 
 
 
@@ -214,11 +214,11 @@
 //Define function traverseObject(someObj)
 //Print every property and it's value.
 
-//function traverseObject(someObj) {
-//    for(var propName in someObj) {
-//        propValue = someObj[propName]
-//        console.log(propName, propValue);
-//    }
+function traverseObject(someObj) {
+    for(let propName in someObj) {
+       propValue = someObj[propName]
+        console.log(propName, propValue);
+    }
 //}
 
 
@@ -229,9 +229,9 @@
 //Print length
 //The lengths should be the same.
 
-//var  = [1, 2, 3, 4, 5, 6];
-//ar.length = 3; 
-//console.log( ar );
+let  = [1, 2, 3, 4, 5, 6];
+ar.length = 3; 
+console.log( ar );
 
 //11. Splice Element
 //Define function spliceElement(someArr)
@@ -240,8 +240,8 @@
 //Print length
 //The lengths should be one less than the original length.
 
-//var arr = [1, 2, 3, 4, 5, 6, 7, ];
-//var removed = arr.splice(3);
+let arr = [1, 2, 3, 4, 5, 6, 7, ];
+let removed = arr.splice(3);
 
 //
 //12. Defining an object using a constructor
@@ -249,14 +249,14 @@
 //The following line should set a Person object to the variable john:
 //	var john = new Person("John", 30);
 
-//let Person = function(name, job, age){
-//	  this.name = name;
-//	}
-//const john = new Person('John');
-//	john.job = 'Developer';
-//	john.age = 30;
-//
-//	console.log(JSON.stringify(john, null, '  '));
+let Person = function(name, job, age){
+	  this.name = name;
+	}
+const john = new Person('John');
+	john.job = 'Developer';
+	john.age = 30;
+
+	console.log(JSON.stringify(john, null, '  '));
 
 //
 //13. Defining an object using an object literal
