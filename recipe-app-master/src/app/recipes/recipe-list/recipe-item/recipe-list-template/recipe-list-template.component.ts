7d31f72bb8d8d795423a3b22c9432cbdb5000986
unknown <amilevich@yahoo.com>
2019-09-05 15:42:58 -0400

    hello hibernate

--- recipe-app-master/src/app/recipes/recipe-list/recipe-item/recipe-list-template/recipe-list-template.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-recipe-list-template',
-  templateUrl: './recipe-list-template.component.html',
-  styleUrls: ['./recipe-list-template.component.css']
-})
-export class RecipeListTemplateComponent {}
